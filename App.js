09962d49de8571d8ec8e46efc7a4b5771b1ab9fe
Gui <marinho.silva.guilherme@escola.pr.gov.br>
2025-10-23 13:00:43 +0000

    a

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import{ScrollView} from 'react-native';
+import Titulo from './Componentes/Titulo';
 
 export default function App(){
   return(
     <ScrollView style={{ padding:20 }}>
-                        
+
 
     </ScrollView>
   )
