bead2410128d478a739619a6e2356a5d44cd30bd
Gui <marinho.silva.guilherme@escola.pr.gov.br>
2025-10-23 13:54:29 +0000

    a

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import{ScrollView} from 'react-native';
 import Titulo from './Componentes/Titulo';
+import Saudacao from './Componentes/Saudacao';
 
 export default function App(){
   return(
     <ScrollView style={{ padding:20 }}>
     <Titulo />
+    <Saudacao nome="Gui"/>
 
     </ScrollView>
   )
