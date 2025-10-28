a6e435dd1b632156cf4ecdc902e0244d7cc024d8
Gui <marinho.silva.guilherme@escola.pr.gov.br>
2025-10-28 11:18:33 +0000

    a

--- Componentes/CartaoUsuario.js
+++ Componentes/CartaoUsuario.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 
-export default function CartaoUsuario({nome, descricao, imagem}){
-    return(
+export default function CartaoUsuario({ nome, descricao, imagem }) {
+    return (
         <View style={estilos.cartao}>
-            <Image source={{uri:imagem}} style={estilos.foto}/>
+            <Image source={{ uri: imagem }} style={estilos.foto} />
             <Text style={estilos.nome}>{nome}</Text>
             <Text style={estilos.descricao}>{descricao}</Text>
 
@@ -13,19 +13,33 @@ export default function CartaoUsuario({nome, descricao, imagem}){
 }
 
 const estilos = StyleSheet.create({
-    cartao:{
-        backgroundColor:'#FFF',
-        padding:16,
-        marginTop:15,
-        borderRadius:10,
-        alignItems:'center',
-        shadowColor:'#000',
+    cartao: {
+        backgroundColor: '#FFF',
+        padding: 16,
+        marginTop: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+        shadowColor: '#000',
         shadowOpacity: 0.1,
-        shadowOffset:{width:0, height:2},
-        shadowRadius:5,
-        elevation:5,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 5,
+        elevation: 5,
     },
-    foto:{width:80, height:80, borderRadius:40, marginBottom:10,},
-    nome:{fontSize:18, fontWeight:'bold',},
-    descricao:{fontSize:14, color:'#555', textAlign:'center',},
+    foto: {
+        width: 80,
+        height: 80,
+        borderRadius: 40,
+        marginBottom: 10,
+    },
+
+    nome: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+
+    descricao: {
+        fontSize: 14,
+        color: '#555',
+        textAlign: 'center',
+    },
 })
