b90602bc44725cf2ea1a949c315f5a84209920a4
Gui <marinho.silva.guilherme@escola.pr.gov.br>
2025-10-30 13:58:02 +0000

    a

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import{ScrollView} from 'react-native';
 import Titulo from './Componentes/Titulo';
 import Saudacao from './Componentes/Saudacao';
 import FotoPerfil from '/Componentes/FotoPerfil';
+import CartaoUsuario from '/Componentes/CartaoUsuario';
 
 export default function App(){
   return<ScrollView>
     <Titulo />
     <Saudacao nome = 'Guilherme' />
-
-
+    <FotoPerfil />
+    <CartaoUsuario nome = 'Saulo da quebrada' descricao = 'Jogador de futebolas' img = 'perfil00' />
     </ScrollView>
   
 }
