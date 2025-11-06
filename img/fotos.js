97c9d481c486e22d68a9cad669b4013d0e2cd0c7
Gui <marinho.silva.guilherme@escola.pr.gov.br>
2025-11-06 12:32:31 +0000

    a

--- img/fotos.js
+++ img/fotos.js
@@ -3,6 +3,6 @@ export default{
     perfil01: require('./2.jpg'),
     perfil02: require('./3.jpg'),
     perfil03: require('./4.jpg'),
-    perfil04: require('/5.jpg'),
+    perfil04: require('./5.jpg'),
     perfilBase: require('./base.jpg'),
 };
